7fd71047a89b3b783c8650ad2cc3cd1f54f0cf28
SakuraJinkyu <122227601+SakuraJinkyu@users.noreply.github.com>
2025-03-21 19:16:16 -0400

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { MapHomeComponent } from './map-home/map-home.component';
-
-export const routes: Routes = [
-    { path: 'map', component: MapHomeComponent },
-    { path: '', redirectTo: '/map', pathMatch: 'full' }
-];
